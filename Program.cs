117d3b873025cecf790f31ec2f87c1f131663eee
Beatriz Batista <byah24@outlook.com>
2025-03-28 16:28:06 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿decimal salario = 0, fgts = 0;
+
+Console.WriteLine("Olá, digite o salário do seu funcionário:");
+salario = Convert.ToDecimal(Console.ReadLine()!);
+
+fgts = (salario/100) * 8;
+
+Console.WriteLine($"\nSalário (R$)..:{salario:N2}");
+Console.Write($"\nFGTS:R$ {fgts :N2}");
